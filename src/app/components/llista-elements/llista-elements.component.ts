5a5328755e172ddf56a6e5b8b911c5cf20311b37
LukaNozadzeIOC <lukassus28@gmail.com>
2026-03-26 21:51:32 +0100

    agregat llista d'elements

--- src/app/components/llista-elements/llista-elements.component.ts
+++ src/app/components/llista-elements/llista-elements.component.ts
@@ -1,13 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { Element } from '../../models/element.model';
+import { CommonModule } from '@angular/common';
+import { TargetaElementComponent } from "../targeta-element/targeta-element.component";
 
 @Component({
   selector: 'app-llista-elements',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TargetaElementComponent],
   templateUrl: './llista-elements.component.html',
   styleUrl: './llista-elements.component.scss'
 })
 export class LlistaElementsComponent {
   @Input() elements: Element[] = []; 
+  trackById(index: number, item: any): number {
+  return item.id;
+}
 }
--- src/app/components/targeta-element/targeta-element.component.ts
+++ src/app/components/targeta-element/targeta-element.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Element } from '../../models/element.model';
+import { UpperCasePipe } from '@angular/common'; 
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-targeta-element',
   templateUrl: './targeta-element.component.html',
   standalone: true,
+  imports: [UpperCasePipe, DecimalPipe]
 })
 export class TargetaElementComponent {
   @Input() element!: Element;
